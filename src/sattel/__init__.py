061a665248e48e2ef37863ff1cbd4bb40c3101f0
weisbrja@x1 <weisbrja@protonmail.com>
2024-10-17 23:34:33 +0200

    temporarily change exclusive output to noop instead of fail

--- src/sattel/__init__.py
+++ src/sattel/__init__.py
@@ -138,13 +138,14 @@ def quiet_pferd():
     def download_bar(self, action: str, path: str) -> ContextManager[ProgressBar]:
         return progress_bar("download", path)
 
-    def fail(*args, **kwargs):
-        raise RuntimeError("Exclusive log output can't be used.")
+    # def fail(*args, **kwargs):
+    #     raise RuntimeError("Exclusive log output can't be used.")
 
     pferd_log.print = noop
     pferd_log.download_bar = download_bar
     pferd_log.crawl_bar = crawl_bar
-    pferd_log.exclusive_output = fail
+    # TODO: why does this even run?
+    pferd_log.exclusive_output = noop  # should instead be fail
 
 
 def request(subject: str):
